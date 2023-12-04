7d995ad19833ba01b260e25c66aee97905c83e72
OmidSa75 <danestaniaesmi@gmail.com>
2023-12-04 13:39:41 +0330

    perf: load model in the base task class

--- client_request.py
+++ client_request.py
@@ -1,6 +1,7 @@
 import celery
 import redis
-import numpy as np 
+import numpy as np
+import sys
 
 
 if __name__ == "__main__":
@@ -11,9 +12,23 @@ if __name__ == "__main__":
         broker='redis://:Eyval@localhost:6379/1',
         task_default_queue='AIWithCelery',
     )
-    task = celery_app.send_task(
-            'inference_model',
-            queue='AIWithCelery',
-            )
-    result = task.get()
+    if len(sys.argv) > 1:
+        number_of_calls = sys.argv[1]
+        try:
+            number_of_calls = int(number_of_calls)
+        except ValueError:
+            print('Error:: first arg must be integer <Number_Of_Calls>')
+            sys.exit(1)
+    else: 
+        number_of_calls = 1
+    g = celery.group(celery_app.signature('inference_model', queue='AIWithCelery') for _ in range(number_of_calls))
+    # for _ in range(number_of_calls):
+
+    #     task = celery_app.send_task(
+    #             'inference_model',
+    #             queue='AIWithCelery',
+    #             )
+    # result = task.get()
+    task = g.apply_async()
+    task.get()
 
